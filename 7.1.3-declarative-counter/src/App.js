52cf04bb2c68d8319b714b19222b906744199459
Andres24680 <tasboyandresrestrepo@aol.com>
2023-01-17 21:25:49 -0500

    showing 2 ways to make counter go up

--- 7.1.3-declarative-counter/src/App.js
+++ 7.1.3-declarative-counter/src/App.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react'
 
 function App(){
-    let [ counter, changeCounter ] = useState(1)
-    window.changeCounter = changeCounter
+    let [ counter, changeCounter ] = useState(1);
+    window.changeCounter = changeCounter;
     return (
         <div>
-            <h1>{counter}</h1>
+            <h1>{counter}</h1>      
             <h4>{counter}</h4>
         </div>
     )
 }
 
 export default App;
+
+/*
+add another way to be done with a add button added 
+
+function App(){
+    let [ counter, changeCounter ] = useState(1);
+    let newCount=() => setCounter(counter + 1); 
+
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={newCount}>Add</button>
+        </div>
+    )
+}
+
+export default App;
+ */
